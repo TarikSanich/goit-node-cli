0f2b069134985f402108c70f4638ce07d8be1410
Granovskiy Uriy <silaveri777@gmail.net>
2024-05-04 22:46:10 +0300

    2

---
+++ index.js
@@ -0,0 +1,54 @@
+import { program } from "commander";
+import Contacts from "./contacts.js";
+
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const options = program.opts();
+
+// TODO: рефакторити
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      const contacts = await Contacts.listContacts();
+      console.table(contacts);
+      break;
+
+    case "get":
+      // ... id
+      const contact = await Contacts.getContactById(id);
+      console.log(contact);
+      break;
+
+    case "add":
+      // ... name email phone
+      const newContact = await Contacts.addContact(name, email, phone);
+      console.log(newContact);
+      break;
+
+    case "remove":
+      // ... id
+      const deletedContact = await Contacts.removeContact(id);
+      console.log(deletedContact);
+      break;
+    case "update":
+      const updatedContact = await Contacts.updateContact(id, {
+        name,
+        email,
+        phone,
+      });
+      console.log(updatedContact);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(options);
